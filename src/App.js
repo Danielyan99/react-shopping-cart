28b5063a2dbd2d7adc1176ef6ac5c9ab200c7388
Narek Danielyan <danielyan.narek99@gmail.com>
2021-03-29 21:55:45 +0400

    master

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className='grid-container'>
       <header>
         <a href='/'>React Shopping Cart</a>
+        <a href=''>for git</a>
       </header>
       <main>Product List</main>
       <footer>All right is reserved. the footer</footer>
